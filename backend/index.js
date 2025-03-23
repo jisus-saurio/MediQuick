a343184b7cf58d00a9cf955503c6eb48b0e4ded7
jisus-saurio <stanleyarce51@gmail.com>
2025-03-22 18:46:47 -0600

    Aarreglando errores

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import {config} from "./src/config.js";
 
 async function main() {
   app.listen(config.server.PORT);
-  console.log("El servidor se está corriendo en tí...");
+  console.log("El servidor esta activo"); //no habia visto esta pagina una disculpa no vi quien me cambio esto:c
 }
 
 main();
