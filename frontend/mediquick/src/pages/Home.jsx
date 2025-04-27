8abb0069f06cb3fce5d093eb9d415cef373cc7b4
jisus-saurio <stanleyarce51@gmail.com>
2025-04-27 11:28:42 -0600

    ajustes a pagina

--- frontend/mediquick/src/pages/Home.jsx
+++ frontend/mediquick/src/pages/Home.jsx
@@ -1,25 +1,64 @@
-import React from 'react';
-import '../style/home.css'; // Importar el CSS externo
-
+import React, { useState, useEffect } from 'react';
+import '../style/home.css'; // CSS actualizado
+import banner1 from '../img/banner1.jpg';
+import banner2 from '../img/banner2.jpg';
+import banner3 from '../img/banner3.jpg';
+import frascos from '../img/frascos.png';
+import cat2 from '../img/pastillas.jpg';
+import cat3 from '../img/cremas.jpg';
+import prod1 from '../img/prod1.jpg';
+import prod2 from '../img/prod2.jpg';
+import prod3 from '../img/prod3.webp';
 
 function Home() {
-    return (
-      <div className="home-container">
-        <section className="banner"></section>
-  
-        <section className="icons">
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="circle"></div>
-        </section>
-  
-        <section className="cards">
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-        </section>
-      </div>
-    );
-  }
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const images = [banner1, banner2, banner3];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 4000); 
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  return (
+    <div className="home-container">
+      
+      {/* Carrusel */}
+      <section className="banner">
+        <img src={images[currentImage]} alt="Banner" className="banner-image" />
+      </section>
+
+      {/* Categorías */}
+      <h2 className="section-title">Categorías</h2>
+      <section className="icons">
+        <div className="circle">
+          <img src={frascos} alt="Categoría 1" className="circle-image" />
+        </div>
+        <div className="circle">
+          <img src={cat2} alt="Categoría 2" className="circle-image" />
+        </div>
+        <div className="circle">
+          <img src={cat3} alt="Categoría 3" className="circle-image" />
+        </div>
+      </section>
+
+      {/* Productos */}
+      <h2 className="section-title">Productos</h2>
+      <section className="cards">
+        <div className="card">
+          <img src={prod1} alt="Producto 1" className="card-image" />
+        </div>
+        <div className="card">
+          <img src={prod2} alt="Producto 2" className="card-image" />
+        </div>
+        <div className="card">
+          <img src={prod3} alt="Producto 3" className="card-image" />
+        </div>
+      </section>
+    </div>
+  );
+}
+
 export default Home;
